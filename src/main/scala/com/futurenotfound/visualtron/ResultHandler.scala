c275e9a96982c77b08d08d239ca66ccac880a5bc
Sean Parsons <github@futurenotfound.com>
2011-02-17 08:12:41 +0000

    Trying to get maps to work.

--- src/main/scala/com/futurenotfound/visualtron/ResultHandler.scala
+++ src/main/scala/com/futurenotfound/visualtron/ResultHandler.scala
@@ -1,19 +1,29 @@
 package com.futurenotfound.visualtron
 
-import scala.swing.{ListView, Component, Publisher}
+import scala.swing.{ListView, Component, Publisher, Table}
 import scala.collection.JavaConversions._
+import java.util.{Map => JavaMap}
 
 class ResultHandler extends Publisher {
   def handle(result: Any): Unit = {
-    val component: Option[Component] = result match {
+    val component: Option[Component] = identifyHandler(result)
+    println(component)
+    if (component.isDefined) publish(new ComponentLoadedEvent(component.get))
+  }
+
+  private[this] def identifyHandler(result: Any): Option[Component] = {
+    result match {
       case seq: Seq[_] => Some(new ListView(seq))
-      case iterable: java.lang.Iterable[_] => Some(new ListView(asScalaIterable(iterable).toList))
+      case map: Map[_, _] => {
+        val columnTitles = map.keySet.toList
+        Some(new Table(Array(columnTitles.map(key => map(key)).toArray), columnTitles))
+      }
+      case iterable: java.lang.Iterable[_] => identifyHandler(asScalaIterable(iterable).toList)
+      case javaMap: JavaMap => identifyHandler(asScalaMap(javaMap))
       case _ => {
         println(result.asInstanceOf[AnyRef].getClass())
         None
       }
     }
-    println(component)
-    if (component.isDefined) publish(new ComponentLoadedEvent(component.get))
   }
 }
--- src/main/scala/com/futurenotfound/visualtron/Visualtron.scala
+++ src/main/scala/com/futurenotfound/visualtron/Visualtron.scala
@@ -15,9 +15,6 @@ object Visualtron extends SimpleSwingApplication {
   private val resultHandler = new ResultHandler()
   private val loader = new Loader(resultHandler)
 
-  private val locationTextField = new TextField {
-    text = ""
-  }
   private val editorArea = new TextArea {}
   private val editorScrollPane = new ScrollPane {
     contents = editorArea
@@ -27,31 +24,20 @@ object Visualtron extends SimpleSwingApplication {
     leftComponent = editorScrollPane
     rightComponent = browserScrollPane
   }
-  private val topPanel = new BoxPanel(Orientation.Horizontal) {
-    contents += locationTextField
-  }
   private val borderPanel = new BorderPanel {
-    add(topPanel, BorderPanel.Position.North)
     add(splitPane, BorderPanel.Position.Center)
   }
 
-  listenTo(loader, resultHandler, locationTextField.keys, editorArea.keys)
+  listenTo(loader, resultHandler, editorArea.keys)
   reactions += {
-    case KeyPressed(`locationTextField`, Enter, _, _) => loader.load(locationTextField.text)
     case KeyPressed(`editorArea`, Enter, Control, _) => {
       println(editorArea.text)
       loader.loadText(editorArea.text)
     }
-    case loadingURL: LoadingURLEvent => {
-      locationTextField.background = Color.getHSBColor(140, 209, 230)
-      locationTextField.text = loadingURL.url
-    }
     case scriptLoaded: ScriptLoadedEvent => {
-      locationTextField.background = Color.getHSBColor(75, 254, 120)
       editorArea.text = scriptLoaded.script
     }
     case componentLoaded: ComponentLoadedEvent => {
-      locationTextField.background = Color.white
       browserScrollPane.viewportView = componentLoaded.component
     }
   }
